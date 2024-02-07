ebd54d7e48f04a97c9c2e8cabebfc0a4117c94d2
monicanovoag <monica.novoa.g@gmail.com>
2024-02-07 17:57:58 -0300

    modificaciones tablas

--- botApp/admin.py
+++ botApp/admin.py
@@ -11,19 +11,16 @@ class tipousuarioAdmin(admin.ModelAdmin):
     list_display = ("id", "nombre_tipo_usuario")
 
 class audiofonoAdmin(admin.ModelAdmin):
-    list_display = ("id","id_paciente_ingresado","audio_fo","ano_nac","fecha_registro")
+    list_display = ("id","audio_fo","ano_nac","fecha_registro")
 
 class audioPersonaAdmin(admin.ModelAdmin):
     list_display = ("id","audio_us","wsp_usuario","ano_nac","fecha_registro_paciente")
 
-class usuarioAdmin(admin.ModelAdmin):
-    list_display = ("id","nombre_usuario","apellido_usuario","correo_usuario","tipo_usuario")
 
 admin.site.register(genero_usuario,generoAdmin)
 admin.site.register(audio_etiqueta,etiquetaAdmin)
 admin.site.register(audio_fono,audiofonoAdmin)
 admin.site.register(audio_persona,audioPersonaAdmin)
-admin.site.register(usuarioBot,usuarioAdmin)
 admin.site.register(tipo_usuario, tipousuarioAdmin)
 
 
--- botApp/models.py
+++ botApp/models.py
@@ -64,30 +64,17 @@ class audio_persona(models.Model):
     audio_us = models.FileField(upload_to='audios/')
     wsp_usuario = models.IntegerField()
     ano_nac = models.CharField(max_length=10, verbose_name="año_nacimiento")
-    fecha_registro_paciente = models.CharField(max_length=15)
+    fecha_registro_paciente = models.DateTimeField(default=timezone.now)
 
     genero_usuario = models.ForeignKey(genero_usuario, on_delete=models.CASCADE)
     audio_etiqueta = models.ForeignKey(audio_etiqueta, on_delete=models.CASCADE)
 
 class audio_fono(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="id_audio") 
-    id_paciente_ingresado = models.CharField(max_length=10, verbose_name="Id Paciente")
     audio_fo = models.FileField(upload_to='audios/')
     ano_nac = models.CharField(max_length=10, verbose_name="Año Nacimiento")
-    fecha_registro = models.CharField(max_length=15)
+    fecha_registro = models.DateTimeField(default=timezone.now)
 
     genero_usuario = models.ForeignKey(genero_usuario, on_delete=models.CASCADE)
     audio_etiqueta = models.ForeignKey(audio_etiqueta, on_delete=models.CASCADE)
 
-class usuarioBot (models.Model):
-    id = models.AutoField(primary_key=True, verbose_name="id_usuario")
-    nombre_usuario = models.CharField(max_length=20)
-    apellido_usuario = models.CharField(max_length=20)
-    correo_usuario = models.EmailField(max_length=40)
-    contrasena_usuario = models.CharField(max_length=30)
-
-    tipo_usuario = models.ForeignKey(tipo_usuario, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return str(self.id)
