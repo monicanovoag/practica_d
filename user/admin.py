3893a99d45e52308230d043820bed167b85e8802
monicanovoag <monica.novoa.g@gmail.com>
2024-03-19 15:26:43 -0300

    nuevo

---
+++ user/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, tipo_usuario
+
+
+class tipo_usuarioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre_tipo_usuario']
+    search_fields = ['nombre_tipo_usuario']
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'nombre', 'apellido', 'is_staff', 'is_superuser']
+    search_fields = ['email', 'nombre', 'apellido']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información personal', {'fields': ('nombre', 'apellido', 'tipo_usuario')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nombre', 'apellido', 'tipo_usuario', 'password1', 'password2', 'is_staff', 'is_superuser')
+        }),
+    )
+    ordering = ['email']
+
+
+admin.site.register(tipo_usuario)
+admin.site.register(User, CustomUserAdmin)
---
+++ user/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import UserManager, AbstractUser, PermissionsMixin, Group, Permission
+from django.utils import timezone
+# Create your models here.
+
+class tipo_usuario(models.Model):
+
+    id = models.AutoField(primary_key=True, verbose_name="id_tipo_usuario")
+    nombre_tipo_usuario = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre_tipo_usuario
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('El email debe ser obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('El superusuario debe ser staff')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('El superusuario debe ser superusuario')
+        return self._create_user(email, password, **extra_fields)
+    
+class User(AbstractUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    tipo_usuario = models.ForeignKey(tipo_usuario, on_delete=models.CASCADE, related_name='tipo_usuario')
+    username = None
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['nombre', 'apellido', 'tipo_usuario']
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def get_full_name(self):
+        return f'{self.nombre} {self.apellido}'
+    
+    def get_short_name(self):
+        return self.nombre or self.email.split('@')[0]
